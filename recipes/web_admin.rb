9697a2baa241275bb80d7bf7fb60ba421858e0c0
Grant Heffernan <heffergm@gmail.com>
2015-02-03 08:24:36 -0500

    lint

--- recipes/web_admin.rb
+++ recipes/web_admin.rb
@@ -1,32 +1,36 @@
+#
+# Cookbook Name:: apiaxle
+# Recipe:: web_admin
+#
+
 package 'nginx' do
   action :install
 end
 
 service 'nginx' do
-  action [ :enable, :start ]
+  action [:enable]
 end
 
 include_recipe 'git'
 
-git "/usr/local/src/apiaxle-admin" do
-  repository "https://github.com/mapzen/apiaxle-admin.git"
-  reference "master"
-  action :sync
+git '/usr/local/src/apiaxle-admin' do
+  action      :sync
+  repository  'https://github.com/mapzen/apiaxle-admin.git'
+  reference   'master'
 end
 
-template "/etc/nginx/sites-available/admin" do
-  source "apiaxle-admin.conf.erb"
+template '/etc/nginx/sites-available/admin' do
+  source 'apiaxle-admin.conf.erb'
 end
 
-file "/etc/nginx/sites-enabled/default" do
+file '/etc/nginx/sites-enabled/default' do
   action :delete
 end
 
-link "/etc/nginx/sites-enabled/admin" do
-  to "/etc/nginx/sites-available/admin"
+link '/etc/nginx/sites-enabled/admin' do
+  to '/etc/nginx/sites-available/admin'
 end
 
 service 'nginx' do
-  action [ :restart ]
+  action [:restart]
 end
-
