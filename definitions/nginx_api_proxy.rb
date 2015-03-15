ecf050d6cc8757074605cf6921de03e60f9651cc
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2015-03-15 13:27:10 -0400

    set fastly client ip as forwarded_for

--- definitions/nginx_api_proxy.rb
+++ definitions/nginx_api_proxy.rb
@@ -1,8 +1,8 @@
-define :nginx_api_proxy, name: nil, domain: nil, enable: true do
+define :nginx_api_proxy, name: nil, domain: nil, forwared_for: nil, enable: true do
   template "#{node[:nginx][:dir]}/sites-available/#{params[:name]}" do
     source 'proxied-api.conf.erb'
     notifies :reload, 'service[nginx]'
-    variables api_name: params[:name], domain: params[:domain]
+    variables api_name: params[:name], domain: params[:domain], forwarded_for: params[:forwarded_for]
   end
   if params[:enable]
     nginx_site params[:name]
