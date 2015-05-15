a49791babfbf85a51756d6fdd62527497e157f4d
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2015-05-15 09:58:44 -0400

    remove forwarded for which was for fastly which is not a source here

--- definitions/nginx_api_proxy.rb
+++ definitions/nginx_api_proxy.rb
@@ -2,7 +2,7 @@ define :nginx_api_proxy, name: nil, domain: nil, forwared_for: nil, enable: true
   template "#{node[:nginx][:dir]}/sites-available/#{params[:name]}" do
     source 'proxied-api.conf.erb'
     notifies :reload, 'service[nginx]'
-    variables api_name: params[:name], domain: params[:domain], forwarded_for: params[:forwarded_for]
+    variables api_name: params[:name], domain: params[:domain]
   end
   if params[:enable]
     nginx_site params[:name]
