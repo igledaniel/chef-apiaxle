5a8f67dfc6f0bbcb6653f5591438ff365d8f3315
Grant Heffernan <heffergm@gmail.com>
2016-05-16 17:06:03 -0400

    lint

--- attributes/apis.rb
+++ attributes/apis.rb
@@ -1,3 +1,3 @@
 default[:apiaxle][:proxied][:apis]         = {}
 default[:apiaxle][:proxied][:real_ip_from] = nil # trusted cidr
-default[:apiaxle][:proxied][:blocked_ips]  = nil # array 
+default[:apiaxle][:proxied][:blocked_ips]  = nil # array
